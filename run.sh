76daaade9ec693fc66e17510e05b02277f9ab07d
Splinter Suidman <splinter@mannenopdemaan.nl>
2018-12-26 00:05:09 +0100

    Switch run.sh to dracula.

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-open -a XQuartz
-appname=$(fd --max-depth 2 --type d --exec echo "{/}" \; ".*.app" /Applications | sed -e 's/\.app$//' | dmenu -i -l 10 -p Launch -nf "#ebdbb2" -nb "#333333" -sf "#333333" -sb "#ebdbb2")
+# Necessary tools for this script:
+# * fd: https://github.com/sharkdp/fd
+# * kies: https://github.com/JodusNodus/kies
+# kiesflags='-fn "Fira Code" -fs 18 -nf "ebdbb2" -sf "fbf1c7" -nb "282828" -sb "333333"'
+kiesflags='-fn "Fira Code" -fs 18 -nf "f8f8f2" -sf "bd93f9" -nb "282a36" -sb "44475a"'
+appname=$( \
+    fd --max-depth 2 --type d --exec echo "{/}" \; ".*.app" /Applications \
+  | sed -e 's/\.app$//' \
+  | kies -p "Open " -l 15 -pd 5 -w 50 $kiesflags \
+)
 if [ -n "$appname" ]; then
   open -a "$appname"
 fi
