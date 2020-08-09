50cd0ca70b53eb930e656b4322ac8f2ae690c4f2
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-08-09 14:52:27 +0200

    Add some LSP settings

--- configuration/home/haskell.nix
+++ configuration/home/haskell.nix
@@ -95,12 +95,29 @@
 
     lsp-mode = {
       enable = true;
+      after = [ "evil-leader" ];
       command = [ "lsp" ];
+      config = ''
+        (evil-leader/set-key
+          "ll" 'lsp)
+      '';
     };
 
     lsp-ui = {
       enable = true;
+      after = [ "evil-leader" ];
       command = [ "lsp-ui-mode" ];
+      init = ''
+        (setq lsp-ui-doc-enable nil)
+        (setq lsp-ui-doc-position 'top)
+      '';
+
+      config = ''
+        (evil-leader/set-key
+          "ld" 'lsp-ui-doc-glance
+          "lgd" 'lsp-ui-peek-find-definitions
+          "lm" 'lsp-ui-imenu)
+      '';
     };
 
     lsp-haskell = {
