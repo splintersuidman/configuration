1825e0aa58acb6e24fbd6e25a942d00522f71a9e
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-08-15 15:01:03 +0200

    mpv: Set youtube-dl options

--- configuration/home/mpv.nix
+++ configuration/home/mpv.nix
@@ -3,5 +3,8 @@
   programs.mpv = {
     enable = true;
     scripts = with pkgs.mpvScripts; [ mpris ];
+    config = {
+      ytdl-format = "(bestvideo[height<=720]+bestaudio)[ext=webm]/bestvideo[height<=720]+bestaudio/best[height<=720]/bestvideo+bestaudio/best";
+    };
   };
 }
