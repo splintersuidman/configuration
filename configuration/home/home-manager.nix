7279ac4c878b6a6490ca754ed9c0bc7721feee63
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-08-15 10:03:56 +0200

    Pin home-manager

--- configuration/home/home-manager.nix
+++ configuration/home/home-manager.nix
@@ -1,4 +1,10 @@
 { ... }:
+let
+  sources = import ../../nix/sources.nix;
+in
 {
-  programs.home-manager.enable = true;
+  programs.home-manager = {
+    enable = true;
+    path = sources.home-manager.outPath;
+  };
 }
