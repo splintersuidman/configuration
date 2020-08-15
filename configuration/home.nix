a3df08b5339d3d25a1241fe69e86ca4c7fe429df
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-08-15 14:00:48 +0200

    nixpkgs: Use pinned version in system and home configuration

--- configuration/home.nix
+++ configuration/home.nix
@@ -1,5 +1,10 @@
 # Common home configuration that will be imported by the hosts.
-{ ... }:
+{ config, ... }:
+let
+  sources = import ../nix/sources.nix;
+  nixpkgs = import sources.nixpkgs { inherit (config.nixpkgs) config; };
+in
 {
+  _module.args.pkgs = nixpkgs;
   nixpkgs.config = import ./nixpkgs.nix;
 }
--- configuration/system.nix
+++ configuration/system.nix
@@ -1,5 +1,14 @@
 # Common system configuration that will be importer by the hosts.
-{ ... }:
+{ config, ... }:
+let
+  sources = import ../nix/sources.nix;
+  nixpkgs = import sources.nixpkgs { inherit (config.nixpkgs) config; };
+in
 {
+  _module.args.pkgs = nixpkgs;
   nixpkgs.config = import ./nixpkgs.nix;
+  nix.nixPath = [
+    "nixpkgs=${sources.nixpkgs}"
+    "nixos-config=/etc/nixos/configuration.nix"
+  ];
 }
