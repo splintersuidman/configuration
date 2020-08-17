5f84675ff0db17503bca75756d0991e1e36380d8
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-08-17 22:01:39 +0200

    python: Install python language server

--- configuration/home/python.nix
+++ configuration/home/python.nix
@@ -1,6 +1,28 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  pylsWithPackages = pythonPackages: extraPackages:
+    let
+      pyls = pythonPackages.python-language-server;
+    in
+    pkgs.buildEnv {
+      name = "pyls-with-packages-${pyls.version}";
+      paths = lib.closePropagation (extraPackages pythonPackages);
+      pathsToLink = [ "/${pythonPackages.python.sitePackages}" ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        makeWrapper ${pyls}/bin/pyls $out/bin/pyls \
+          --prefix PYTHONPATH : $out/${pythonPackages.python.sitePackages}
+      '';
+    };
+
+  pyls = pylsWithPackages pkgs.python3Packages (pyPkgs: [
+    pyPkgs.pyls-black
+    pyPkgs.pyls-mypy
+  ]);
+in
 {
   home.packages = [
     pkgs.python3
+    pyls
   ];
 }
