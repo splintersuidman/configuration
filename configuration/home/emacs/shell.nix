5e3d0531dd883a25ad2a8e69685b25d8210fd2ad
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-09-08 21:43:55 +0200

    emacs, eshell: Fix eshell-new
    It does not seem to work in the `:init' of `use-package eshell'.

--- configuration/home/emacs/shell.nix
+++ configuration/home/emacs/shell.nix
@@ -1,14 +1,11 @@
 { ... }: {
-  programs.emacs.init.usePackage = {
-    eshell = {
-      init = ''
-        (defun eshell-new ()
-          "Create a new eshell."
-          (interactive)
-          ;; The argument to `eshell' does not really matter, as long as it's
-          ;; not an integer (n), which causes Emacs to open the nth `eshell'.
-          (eshell 'new))
-      '';
-    };
-  };
+  # TODO: can this be moved into a use-package form?
+  programs.emacs.init.postlude = ''
+    (defun eshell-new ()
+      "Create a new eshell."
+      (interactive)
+      ;; The argument to `eshell' does not really matter, as long as it's
+      ;; not an integer (n), which causes Emacs to open the nth `eshell'.
+      (eshell 'new))
+  '';
 }
