e489e3e2c4a7002254a20594d95e0ca30f44ed36
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-09-12 22:47:58 +0200

    discord: Update to v0.0.12

--- configuration/home/chat.nix
+++ configuration/home/chat.nix
@@ -1,8 +1,19 @@
 { pkgs, ... }:
+let
+  # Discord has a habit of disallowing older client versions...
+  discord =
+    let
+      # Nixpkgs for Discord v0.0.12.
+      pkgs' = import (fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/977147bd04b8e48ab54e126b75a20c46c9afc6bb.tar.gz";
+        sha256 = "1h0nfynn4cscvr0a695z0swmjgrglr1z7kp25jqhrpa9rsfyywza";
+      }) { };
+    in pkgs'.discord;
+in
 {
   home.packages = [
     pkgs.signal-desktop
     pkgs.qtox
-    pkgs.discord
+    discord
   ];
 }
