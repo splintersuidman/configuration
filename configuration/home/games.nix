ac5f8b1b6a0b55ab45b094f3080e3a4a67323d69
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-10-19 13:13:18 +0200

    games: Install Minecraft and Steam

--- configuration/home/games.nix
+++ configuration/home/games.nix
@@ -6,11 +6,13 @@
       gtkClient = true;
       sdlClient = false;
     })
+    pkgs.minecraft
     (pkgs.openttd.overrideAttrs (attrs: {
       # Compile with XDG Base Directory support.
       configureFlags = attrs.configureFlags ++ [ "--with-xdg-basedir" ];
       buildInputs = attrs.buildInputs ++ [ pkgs.libxdg_basedir ];
     }))
     pkgs.scid-vs-pc
+    pkgs.steam
   ];
 }
