ac5423154e8e1dbba74289c246fbe222842ec9f1
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-10-29 13:43:22 +0100

    theme: Disable default application options

--- configuration/home/theme.nix
+++ configuration/home/theme.nix
@@ -17,4 +17,14 @@ in {
     inherit kind;
     inherit (theme) name colors;
   };
+
+  # These options default to `true`, but they do and install some things I don't
+  # want, so I disable them.
+  gtk.enableBase16Theme = false;
+  programs.bat.enableBase16Theme = false;
+  programs.gnome-terminal.enableBase16Theme = false;
+  programs.rofi.enableBase16Theme = false;
+  services.dunst.enableBase16Theme = false;
+  services.polybar.enableBase16Theme = false;
+  services.xscreensaver.enableBase16Theme = false;
 }
