eebc603238d4672853ed20dc626a5368d93ccf79
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-11-09 20:55:38 +0100

    xmonad: Configure with home-manager
    Previously, XMonad was enabled in both the NixOS and the home-manager
    configuration. Both just used the default XMonad binary, which caused
    recompiling after login (which was quite frustrating). Now, my XMonad
    build from ~/.xmonad is used and is not recompiled after login.

--- configuration/home/xsession.nix
+++ configuration/home/xsession.nix
@@ -1,16 +1,29 @@
 { pkgs, config, ... }:
 let
   wallpaper = "${config.xdg.userDirs.pictures}/A3MuFbJ.jpg";
-in
-{
+
+  xmonad-splintah =
+    pkgs.callPackage /home/splinter/.xmonad/xmonad-splintah/default.nix {
+      nixpkgs = pkgs;
+    };
+  targetSystem = pkgs.stdenv.targetPlatform.system;
+  # Rename executable to xmonad-${targetSystem} (e.g. xmonad-linux-x86_64) to
+  # prevent XMonad from recompiling after boot.
+  xmonad = pkgs.stdenv.mkDerivation {
+    name = "xmonad";
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ${xmonad-splintah}/bin/xmonad-splintah $out/bin/xmonad-${targetSystem}
+    '';
+  };
+in {
   xsession = {
     enable = true;
     initExtra = ''
       ${pkgs.feh}/bin/feh --no-fehbg --bg-scale ${wallpaper}
     '';
 
-    windowManager = {
-      command = "${pkgs.haskellPackages.xmonad}/bin/xmonad";
-    };
+    windowManager.command = "${xmonad}/bin/xmonad-${targetSystem}";
   };
 }
--- configuration/system/xserver.nix
+++ configuration/system/xserver.nix
@@ -23,7 +23,13 @@
         ${pkgs.lightlocker}/bin/light-locker --lock-on-lid --lock-on-suspend --lock-after-screensaver=0 &
       '';
     };
-    windowManager.xmonad.enable = true;
+
+    # NOTE: I configure XMonad from home-manager, but NixOS requires at least
+    # one desktop manager or window manager to be enabled. See
+    # <https://github.com/nix-community/home-manager/issues/1180#issuecomment-617329614>
+    # and for an alternative solution see
+    # <https://discourse.nixos.org/t/opening-i3-from-home-manager-automatically/4849/8>.
+    desktopManager.xterm.enable = true;
 
     synaptics = {
       horizEdgeScroll = true;
