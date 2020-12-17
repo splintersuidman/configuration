c4deed7fb55f24dc7e65ef26386a7670657a335a
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-12-17 12:45:11 +0100

    git-gutter: Use git-gutter-fringe for prettier git gutters

--- configuration/home/git.nix
+++ configuration/home/git.nix
@@ -23,17 +23,39 @@
       after = [ "evil" "magit" ];
     };
 
-    git-gutter = {
+    git-gutter = let
+      git-gutter-fringe-enabled =
+        config.programs.emacs.init.usePackage.git-gutter-fringe.enable;
+    in {
       enable = true;
+      init = if git-gutter-fringe-enabled then ''
+        (setq git-gutter:init-function 'git-gutter-fr:init
+              git-gitter:view-diff-function 'git-gutter-fr:view-diff-infos
+              git-gutter:clear-function 'git-gutter-fr:clear
+              git-gutter:window-width -1)
+      '' else
+        "";
       config = ''
         (global-git-gutter-mode)
       '';
     };
 
-    browse-at-remote = {
+    git-gutter-fringe = {
       enable = true;
+      after = [ "git-gutter" ];
+      init = ''
+        (setq fringes-outside-margins t)
+      '';
       config = ''
+        (define-fringe-bitmap 'git-gutter-fr:added [224]
+          nil nil '(center repeated))
+        (define-fringe-bitmap 'git-gutter-fr:modified [224]
+          nil nil '(center repeated))
+        (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
+          nil nil 'bottom)
       '';
     };
+
+    browse-at-remote = { enable = true; };
   };
 }
