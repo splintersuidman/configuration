5e7277d47dd45fca410c8e322428ab41caa82f1a
Splinter Suidman <splinter@mannenopdemaan.nl>
2020-12-28 15:22:23 +0100

    eshell: Use use-package form

--- configuration/home/emacs/shell.nix
+++ configuration/home/emacs/shell.nix
@@ -1,11 +1,25 @@
 { ... }: {
-  # TODO: can this be moved into a use-package form?
-  programs.emacs.init.postlude = ''
-    (defun eshell-new ()
-      "Create a new eshell."
-      (interactive)
-      ;; The argument to `eshell' does not really matter, as long as it's
-      ;; not an integer (n), which causes Emacs to open the nth `eshell'.
-      (eshell 'new))
-  '';
+  programs.emacs.init.usePackage = {
+    eshell = {
+      enable = true;
+      after = [ "general" ];
+      init = ''
+        (defun eshell-new ()
+          "Create a new eshell."
+          (interactive)
+          ;; The argument to `eshell' does not really matter, as long as it's
+          ;; not an integer (n), which causes Emacs to open the nth `eshell'.
+          (eshell 'new))
+      '';
+      hook = [''
+        (eshell-mode .
+          (lambda ()
+            (general-define-key
+              :prefix my-local-leader
+              :keymaps 'eshell-mode-map
+              :states '(normal visual motion)
+              "h" 'counsel-esh-history)))
+      ''];
+    };
+  };
 }
