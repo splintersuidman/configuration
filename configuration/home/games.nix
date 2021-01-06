ab881207ee3d7893d946a26d2e5c1d14bd296950
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-01-06 21:32:09 +0100

    games: Uninstall desmume, freeciv, openttd, scid-vs-pc

--- configuration/home/games.nix
+++ configuration/home/games.nix
@@ -1,18 +1,18 @@
 { pkgs, ... }: {
   home.packages = [
-    pkgs.desmume
-    (pkgs.freeciv.override {
-      gtkClient = true;
-      sdlClient = false;
-    })
+    # pkgs.desmume
+    # (pkgs.freeciv.override {
+    #   gtkClient = true;
+    #   sdlClient = false;
+    # })
     pkgs.minecraft
     pkgs.multimc
-    (pkgs.openttd.overrideAttrs (attrs: {
-      # Compile with XDG Base Directory support.
-      configureFlags = attrs.configureFlags ++ [ "--with-xdg-basedir" ];
-      buildInputs = attrs.buildInputs ++ [ pkgs.libxdg_basedir ];
-    }))
-    pkgs.scid-vs-pc
+    # (pkgs.openttd.overrideAttrs (attrs: {
+    #   # Compile with XDG Base Directory support.
+    #   configureFlags = attrs.configureFlags ++ [ "--with-xdg-basedir" ];
+    #   buildInputs = attrs.buildInputs ++ [ pkgs.libxdg_basedir ];
+    # }))
+    # pkgs.scid-vs-pc
     pkgs.steam
   ];
 }
