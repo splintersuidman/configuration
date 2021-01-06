f3f1204af9297865d204ce4a5a91c1cf926ee4ed
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-01-06 21:28:35 +0100

    postgresql: Install

--- hosts/thinkpad/system.nix
+++ hosts/thinkpad/system.nix
@@ -8,6 +8,7 @@
     ../../configuration/system/autorandr.nix
     ../../configuration/system/gtk.nix
     ../../configuration/system/nix.nix
+    ../../configuration/system/postgresql.nix
     ../../configuration/system/printing.nix
     ../../configuration/system/shell.nix
     ../../configuration/system/tlp.nix
