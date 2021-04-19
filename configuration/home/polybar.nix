2169f08d82c2df3195cd80399835efe053bb4c51
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-04-19 21:11:11 +0200

    polybar: Update
    Use material design icons instead of text labels. Don't use underline
    per module.
    
    Use xwindow module instead of XMonad logging for focused window title,
    since there were some Unicode problems. Update XMonad input
    accordingly.

--- configuration/home/polybar.nix
+++ configuration/home/polybar.nix
@@ -1,16 +1,14 @@
 { pkgs, config, ... }:
-let
-  colors = config.theme.base16.colors;
-in
-{
+let colors = config.theme.base16.colors;
+in {
+  home.packages = [ pkgs.material-icons ];
+
   services.polybar = {
     enable = true;
     package = pkgs.polybar.override { mpdSupport = true; };
     script = "${config.services.polybar.package}/bin/polybar splintah &";
     config = {
-      settings = {
-        screenchange-reload = true;
-      };
+      settings = { screenchange-reload = true; };
 
       "global/wm" = {
         margin-top = 0;
@@ -19,14 +17,14 @@ in
 
       "bar/splintah" = {
         width = "100%";
-        height = 20;
+        height = 19;
         radius = 0;
         fixed-center = false;
 
         background = "#${colors.base00.hex.rgb}";
         foreground = "#${colors.base05.hex.rgb}";
 
-        line-size = 2;
+        line-size = 0;
         line-color = "#${colors.base00.hex.rgb}";
         padding-left = 1;
         padding-right = 1;
@@ -34,14 +32,17 @@ in
         module-margin-left = 1;
         module-margin-right = 1;
 
-        modules-left = [ "xmonad" ];
+        modules-left = [ "xmonad" "window" ];
         modules-center = [ ];
-        modules-right = [ "alsa" "xkeyboard" "memory" "cpu" "eth" "wlan" "battery" "date" ];
+        modules-right =
+          [ "alsa" "xkeyboard" "memory" "cpu" "wlan" "battery" "date" ];
 
-        font-0 = "Iosevka Custom:pixelsize=10:antialias=true:autohint=true";
-        font-1 = "Iosevka Custom:style=Bold:pixelsize=10:antialias=true:autohint=true";
-        font-2 = "unifont:fontformat=truetype:size=8:antialias=false;0";
-        font-3 = "siji:pixelsize=10;1";
+        font-0 =
+          "Iosevka Custom:style=Medium:pixelsize=10:antialias=true:autohint=true";
+        font-1 =
+          "Iosevka Custom:style=Bold:pixelsize=10:antialias=true:autohint=true";
+        font-2 = "Iosevka Aile:pixelsize=10:antialias=true:autohint=true";
+        font-3 = "Material Icons:pixelsize=12;2";
 
         tray-position = "none";
         tray-padding = 2;
@@ -56,7 +57,7 @@ in
 
       "module/xmonad" = {
         type = "custom/script";
-        exec = "${pkgs.coreutils}/bin/tail -F /tmp/.xmonad-log";
+        exec = "${pkgs.coreutils}/bin/tail -f /tmp/.xmonad-log";
         exec-if = "[ -p /tmp/.xmonad-log ]";
         tail = true;
       };
@@ -65,26 +66,25 @@ in
         type = "internal/xkeyboard";
         blacklist-0 = "num lock";
 
-        format-prefix = "%{A1:${config.home.homeDirectory}/.local/bin/switch-keyboard.sh:}%{T1}KB%{T-} ";
+        format-prefix =
+          "%{A1:${config.home.homeDirectory}/.local/bin/switch-keyboard.sh:} ";
         format-suffix = "%{A}";
-        format-prefix-foreground = "#${colors.base05.hex.rgb}";
-        format-prefix-underline = "#${colors.base0B.hex.rgb}";
+        format-prefix-foreground = "#${colors.base0B.hex.rgb}";
 
         label-layout = "%layout%";
-        label-layout-underline = "#${colors.base0B.hex.rgb}";
 
         label-indicator-padding = 2;
         label-indicator-margin = 1;
         label-indicator-background = "#${colors.base0C.hex.rgb}";
-        label-indicator-underline = "#${colors.base0B.hex.rgb}";
       };
 
       "module/mpd" = {
         type = "internal/mpd";
-        format-online = "<toggle> %{A1:${config.programs.urxvt.package}/bin/urxvt -e ${pkgs.ncmpcpp}/ bin/ncmpcpp:}<label-song>%{A}";
+        format-online =
+          "<toggle> %{A1:${config.programs.urxvt.package}/bin/urxvt -e ${pkgs.ncmpcpp}/ bin/ncmpcpp:}<label-song>%{A}";
 
-        icon-play = "▶";
-        icon-pause = "⏸";
+        icon-play = "";
+        icon-pause = "";
 
         label-song = "%title% - %artist%";
         label-song-maxlen = 35;
@@ -99,20 +99,20 @@ in
       "module/cpu" = {
         type = "internal/cpu";
         interval = 2;
-        format-prefix = "%{A1:${config.programs.urxvt.package}/bin/urxvt -e ${pkgs.htop}/bin/htop --sort-key PERCENT_CPU:}%{T1}CPU%{T-} ";
+        format-prefix =
+          "%{A1:${config.programs.urxvt.package}/bin/urxvt -e ${pkgs.htop}/bin/htop --sort-key PERCENT_CPU:} ";
         format-suffix = "%{A}";
-        format-prefix-foreground = "#${colors.base04.hex.rgb}";
-        format-underline = "#${colors.base08.hex.rgb}";
+        format-prefix-foreground = "#${colors.base08.hex.rgb}";
         label = "%percentage:2%%";
       };
 
       "module/memory" = {
         type = "internal/memory";
         interval = 2;
-        format-prefix = "%{A1:${config.programs.urxvt.package}/bin/urxvt -e ${pkgs.htop}/bin/htop --sort-key PERCENT_MEM:}%{T1}MEM%{T-} ";
+        format-prefix =
+          "%{A1:${config.programs.urxvt.package}/bin/urxvt -e ${pkgs.htop}/bin/htop --sort-key PERCENT_MEM:} ";
         format-suffix = "%{A}";
-        format-prefix-foreground = "#${colors.base04.hex.rgb}";
-        format-underline = "#${colors.base0D.hex.rgb}";
+        format-prefix-foreground = "#${colors.base0E.hex.rgb}";
         label = "%percentage_used%%";
       };
 
@@ -120,30 +120,23 @@ in
         type = "internal/network";
         interval = "3.0";
 
-        format-connected = "<ramp-signal> <label-connected>";
-        format-connected-underline = "#${colors.base0C.hex.rgb}";
+        format-connected = "<label-connected>";
         label-connected = "%essid%";
         format-connected-prefix =
-          "%{A1:${config.programs.urxvt.package}/bin/urxvt -e ${pkgs.networkmanager}/bin/nmtui:}%{T1}";
-        format-connected-suffix = "%{T-}%{A}";
+          "%{A1:${config.programs.urxvt.package}/bin/urxvt -e ${pkgs.networkmanager}/bin/nmtui:} ";
+        format-connected-prefix-foreground = "#${colors.base0C.hex.rgb}";
+        format-connected-suffix = "%{A}";
 
-        format-disconnected = "";
-
-        ramp-signal-0 = "WLAN";
-        ramp-signal-1 = "%{F#${colors.base05.hex.rgb}}W%{F#${colors.base04.hex.rgb}}LAN";
-        ramp-signal-2 = "%{F#${colors.base05.hex.rgb}}WL%{F#${colors.base04.hex.rgb}}AN";
-        ramp-signal-3 = "%{F#${colors.base05.hex.rgb}}WLA%{F#${colors.base04.hex.rgb}}N";
-        ramp-signal-4 = "%{F#${colors.base05.hex.rgb}}WLAN%{F#${colors.base04.hex.rgb}}";
-        ramp-signal-foreground = "#${colors.base04.hex.rgb}";
+        format-disconnected = "";
+        format-disconnected-foreground = "#${colors.base08.hex.rgb}";
       };
 
       "module/eth" = {
         type = "internal/network";
         interval = "3.0";
 
-        format-connected-underline = "#${colors.base0E.hex.rgb}";
-        format-connected-prefix = "ETH ";
-        format-connected-prefix-foreground = "#${colors.base04.hex.rgb}";
+        format-connected-prefix = " ";
+        format-connected-prefix-foreground = "#${colors.base0E.hex.rgb}";
         label-connected = "%local_ip%";
 
         format-disconnected = "";
@@ -159,7 +152,8 @@ in
         time = "%H:%M";
         time-alt = " %H:%M:%S";
 
-        format-underline = "#${colors.base0D.hex.rgb}";
+        format-prefix = " ";
+        format-prefix-foreground = "#${colors.base0D.hex.rgb}";
 
         label = "%date%%time%";
       };
@@ -167,13 +161,15 @@ in
       "module/alsa" = {
         type = "internal/alsa";
 
-        format-volume = "<label-volume> <bar-volume>";
-        label-volume = "%{T1}VOL%{T-}";
-        label-volume-foreground = "#${colors.base04.hex.rgb}";
+        format-volume = "<ramp-volume> <bar-volume>";
+        ramp-volume-0 = "";
+        ramp-volume-1 = "";
+        ramp-volume-2 = "";
+        ramp-volume-foreground = "#${colors.base0D.hex.rgb}";
 
         format-muted-prefix = " ";
-        format-muted-foreground = "#${colors.base04.hex.rgb}";
-        label-muted = "geluid uit";
+        format-muted-foreground = "#${colors.base08.hex.rgb}";
+        label-muted = "";
 
         bar-volume-width = 8;
         bar-volume-foreground-0 = "#${colors.base0B.hex.rgb}";
@@ -195,34 +191,53 @@ in
 
       "module/battery" = {
         type = "internal/battery";
-        full-at = 98;
 
-        format-charging = "%{T1}<animation-charging> <label-charging>%{T-}";
-        format-charging-underline = "#${colors.base0B.hex.rgb}";
+        format-charging = "<label-charging>";
+        format-charging-prefix = " ";
+        format-charging-prefix-foreground = "#${colors.base0B.hex.rgb}";
 
-        format-discharging = "%{T1}<animation-discharging> <label-discharging>%{T-}";
-        format-discharging-underline = "#${colors.base0A.hex.rgb}";
+        format-discharging = "<label-discharging>";
+        format-discharging-prefix = " ";
+        format-discharging-prefix-foreground = "#${colors.base0A.hex.rgb}";
 
-        format-full-prefix = "≡ ";
-        format-full-prefix-foreground = "#${colors.base04.hex.rgb}";
-        format-full-underline = "#${colors.base0B.hex.rgb}";
+        format-full-prefix = " ";
+        format-full-prefix-foreground = "#${colors.base0B.hex.rgb}";
+      };
 
-        ramp-capacity-0 = "";
-        ramp-capacity-1 = "";
-        ramp-capacity-2 = "";
-        ramp-capacity-foreground = "#${colors.base04.hex.rgb}";
+      # NOTE: missing with respect to logging with XMonad:
+      # - filter out NSP
+      # - filter out workspaces without windows, not really necessary
+      "module/workspaces" = {
+        type = "internal/xworkspaces";
 
-        animation-charging-0 = "↑";
-        animation-charging-1 = "↑";
-        animation-charging-2 = "↑";
-        animation-charging-foreground = "#${colors.base04.hex.rgb}";
-        animation-charging-framerate = 750;
+        enable-click = true;
+        enable-scroll = true;
 
-        animation-discharging-0 = "↓";
-        animation-discharging-1 = "↓";
-        animation-discharging-2 = "↓";
-        animation-discharging-foreground = "#${colors.base04.hex.rgb}";
-        animation-discharging-framerate = 750;
+        format = "<label-state>";
+
+        label-active = "%name%";
+        label-active-foreground = "#${colors.base00.hex.rgb}";
+        label-active-background = "#${colors.base0C.hex.rgb}";
+        label-active-padding = 1;
+
+        label-occupied = "%name%";
+        label-occupied-padding = 1;
+
+        label-urgent = "%name%";
+        label-urgent-foreground = "#${colors.base00.hex.rgb}";
+        label-urgent-background = "#${colors.base07.hex.rgb}";
+        label-urgent-padding = 1;
+
+        label-empty = "%name%";
+        label-empty-padding = 1;
+      };
+
+      "module/window" = {
+        type = "internal/xwindow";
+
+        format = "<label>";
+        format-foreground = "#${colors.base07.hex.rgb}";
+        label = "%title%";
       };
     };
   };
