6cd8088b239de8f38b8054b6450d71bb4f07d56a
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-05-04 17:50:37 +0200

    mpv: Set font

--- configuration/home/mpv.nix
+++ configuration/home/mpv.nix
@@ -1,10 +1,11 @@
-{ pkgs, config, ... }:
-{
+{ pkgs, config, ... }: {
   programs.mpv = {
     enable = true;
     scripts = with pkgs.mpvScripts; [ mpris ];
     config = {
-      ytdl-format = "(bestvideo[height<=720]+bestaudio)[ext=webm]/bestvideo[height<=720]+bestaudio/best[height<=720]/bestvideo+bestaudio/best";
+      ytdl-format =
+        "(bestvideo[height<=720]+bestaudio)[ext=webm]/bestvideo[height<=720]+bestaudio/best[height<=720]/bestvideo+bestaudio/best";
+      osd-font = "Iosevka Aile";
     };
   };
 }
