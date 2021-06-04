c01e45adb7b0ee09327652572759adffca08b6b6
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-06-04 23:27:00 +0200

    gtk: Fix dconf configuration

--- configuration/system/gtk.nix
+++ configuration/system/gtk.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   # Necessary for home-manager GTK configuration, see
   # <https://github.com/rycee/home-manager/blob/1afa5e257b71e1d27c6fc80ca38f344ea228bc1b/doc/faq.adoc#why-do-i-get-an-error-message-about-cadesrtdconf>.
-  services.dbus.packages = [ pkgs.gnome3.dconf ];
+  programs.dconf.enable = true;
 }
