2ecf50470f4da30903cd9e14ca6706bdad195114
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-08-11 17:41:09 +0200

    project.el: Add keybindings and projects with a .project file
    Recognise directories with a .project file as a project.

--- configuration/home/emacs/init/project.el
+++ configuration/home/emacs/init/project.el
@@ -1,9 +1,46 @@
+(require 'init-keybindings)
+
 (use-package project
+  :after (general)
   :init
   (defun splinter-project-root ()
     "Return the project root directory or ‘nil’ if there is no current project."
     (let ((project (project-current)))
       (when project
-        (project-root project)))))
+        (project-root project))))
+
+  (cl-defmethod project-root ((project (head splinter-dot-project)))
+    (cdr project))
+
+  (defun splinter-project-try-dot-project (dir)
+    "Try to find a .project file in ‘dir’ indicating a project directory."
+    (let ((root (locate-dominating-file dir ".project")))
+      (and root (cons 'splinter-dot-project root))))
+  :custom
+  (project-switch-commands '((project-find-file "Find file" ?f)
+                             (project-dired "Dired" ?d)
+                             (project-eshell "Eshell" ?e)
+                             (splinter-project-vterm "Vterm" ?v)
+                             (project-execute-extended-command "Execute command" ?x)
+                             (magit-project-status "Magit" ?g)))
+  (project-find-functions '(splinter-project-try-dot-project project-try-vc))
+  :general
+  (my-leader-def
+    "p!" '(project-shell-command :which-key "Shell command")
+    "p&" '(project-async-shell-command :which-key "Async shell command")
+    "pF" '(project-or-external-find-file :which-key "Or external find file")
+    "pG" '(project-or-external-find-regexp :which-key "Or external find regexp")
+    "pb" '(project-switch-to-buffer :which-key "Switch to buffer")
+    "pc" '(project-compile :which-key "Compile")
+    "pd" '(project-dired :which-key "Dired")
+    "pe" '(project-eshell :which-key "Eshell")
+    "pf" '(project-find-file :which-key "Find file")
+    "pg" '(project-find-regexp :which-key "Find regexp")
+    "pk" '(project-kill-buffers :which-key "Kill buffers")
+    "pm" '(magit-project-status :which-key "Magit status")
+    "pp" '(project-switch-project :which-key "Switch project")
+    "ps" '(project-eshell :which-key "Shell")
+    "pv" '(splinter-project-vterm :which-key "Vterm")
+    "px" '(project-execute-extended-command :which-key "Execute command")))
 
 (provide 'init-project)
