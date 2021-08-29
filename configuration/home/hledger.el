8455b0bcd1bcab399c5b8e5a8d7ad5f15540cf0f
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-08-29 19:18:55 +0200

    hledger: Use hledger-mode in Emacs instead of ledger-mode
    Leder-mode stopped working because it uses the flag --date-format
    which is not implemented by hledger.

--- configuration/home/hledger.el
+++ configuration/home/hledger.el
@@ -1,10 +1,11 @@
 (require 'init-keybindings)
 
 (use-package ledger-mode
+  :disabled
   :ensure t
   :after general
   :mode
-  "\\.leder\\'"
+  "\\.ledger\\'"
   "\\.journal\\'"
   :custom
   (ledger-binary-path "hledger")
@@ -31,5 +32,30 @@
     "s" 'ledger-sort-region
     "t" 'ledger-insert-effective-date
     "u" 'ledger-schedule-upcoming))
+
+(use-package hledger-mode
+  :ensure t
+  :mode
+  "\\.ledger\\'"
+  "\\.journal\\'"
+  :custom
+  (hledger-jfile "~/.hledger.journal")
+  (hledger-comments-column 4)
+  (hledger-currency-string "€")
+  :init
+  (defun splinter-hledger-setup-completion ()
+    (make-local-variable 'completion-at-point-functions)
+    (add-to-list 'completion-at-point-functions 'hledger-completion-at-point))
+  :hook
+  (hledger-mode . splinter-hledger-setup-completion)
+  :general
+  (my-local-leader-def
+    :keymaps 'hledger-mode-map
+    "TAB" '(hledger-append-clipboard-to-journal :which-key "Append clipboard to journal")
+    "b" '(hledger-edit-amount :which-key "Edit amount")
+    "d" '(hledger-reschedule :which-key "Reschedule")
+    "n" '(hledger-next-or-new-entry :which-key "Next or new entry")
+    "p" '(hledger-backward-entry :which-key "Previous entry")
+    "r" '(hledger-run-command :which-key "Run command")))
 
 (provide 'init-ledger)
