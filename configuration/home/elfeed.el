c4a7b752045c84de4a87134f627f5716e89ae799
Splinter Suidman <splinter@mannenopdemaan.nl>
2021-09-04 22:39:04 +0200

    elfeed: Fix style

--- configuration/home/elfeed.el
+++ configuration/home/elfeed.el
@@ -9,13 +9,18 @@
     "Set elfeed faces for base16-themes."
     (when (or force
               (and (featurep 'elfeed)
-                   (-any? (lambda (theme)
-                            (string-prefix-p "base16-" (symbol-name theme)))
-                          custom-enabled-themes)))
-      (message "Setting fonts")
-      (set-face-attribute 'elfeed-search-feed-face nil :inherit font-lock-function-name-face :foreground nil)
-      (set-face-attribute 'elfeed-search-tag-face nil :inherit font-lock-type-face :foreground nil)
-      (set-face-attribute 'elfeed-search-date-face nil :inherit font-lock-variable-name-face :foreground nil)))
+                   (cl-some (lambda (theme)
+                              (string-prefix-p "base16-" (symbol-name theme)))
+                            custom-enabled-themes)))
+      (set-face-attribute 'elfeed-search-feed-face nil
+                          :inherit font-lock-function-name-face
+                          :foreground nil)
+      (set-face-attribute 'elfeed-search-tag-face nil
+                          :inherit font-lock-type-face
+                          :foreground nil)
+      (set-face-attribute 'elfeed-search-date-face nil
+                          :inherit font-lock-variable-name-face
+                          :foreground nil)))
   :config
   (defun splinter-elfeed-mpv (entry)
     "Open the link of the currently selected item in mpv."
