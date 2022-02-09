4a5a655f397d9afaa83195936c2e5f1f7847ca16
Splinter Suidman <splinter@mannenopdemaan.nl>
2022-02-09 11:03:20 +0100

    digital: Install

--- configuration/home/misc.nix
+++ configuration/home/misc.nix
@@ -1,9 +1,19 @@
 { pkgs, ... }:
-{
+let
+  digital = pkgs.nur.repos.syberant.digital.overrideAttrs (old: {
+    # Fix the fonts.
+    installPhase = ''
+      ${old.installPhase}
+      makeWrapper $out/share/Digital/Digital/Digital.sh $out/bin/Digital \
+        --set _JAVA_OPTIONS "-Dawt.useSystemAAFontSettings=lcd"
+    '';
+  });
+in {
   home.packages = [
     pkgs.libreoffice
     pkgs.anki
-    pkgs.wineFull
+    pkgs.wineWowPackages.stable
     pkgs.musescore
+    digital
   ];
 }
