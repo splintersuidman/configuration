f4a78f0283ecf224d8a26d4662c43ab6823e4e91
Splinter Suidman <splinter@mannenopdemaan.nl>
2022-02-09 11:07:35 +0100

    fonts: Install some fonts

--- configuration/home/fonts.nix
+++ configuration/home/fonts.nix
@@ -1,25 +1,52 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+let
+  charter = with pkgs;
+    let version = "210112";
+    in fetchzip rec {
+      name = "charter-${version}";
+
+      url = "https://practicaltypography.com/fonts/Charter%20${version}.zip";
+      sha256 = "sha256-hBDXVRMTFGQ9WOCa1qnUcGpKs8y4waVKVlEta3fGaYI=";
+
+      postFetch = ''
+        mkdir -p $out/share/{doc,fonts}
+        unzip -j $downloadedFile \*.ttf -d $out/share/fonts/truetype
+        unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
+        unzip -j $downloadedFile \*license.txt -d $out/share/doc/${name}
+      '';
+    };
+in {
   fonts.fontconfig.enable = true;
 
-  imports = [
-    ./fonts/iosevka.nix
-  ];
+  imports = [ ./fonts/iosevka.nix ];
 
   home.packages = with pkgs; [
+    charis-sil
+    charter
+    cooper-hewitt
+    crimson
+    dejavu_fonts
+    eb-garamond
     fantasque-sans-mono
     fira
     fira-code
     fira-mono
     font-awesome-ttf
+    gentium
+    ibm-plex
     inconsolata
     libertine
-    symbola
-    dejavu_fonts
-    siji
-    unifont
-    crimson
-    ibm-plex
     mononoki
+    noto-fonts
+    roboto
+    roboto-mono
+    roboto-slab
+    siji
+    source-code-pro
+    source-sans
+    source-serif
+    symbola
+    unifont
 
     fontforge-gtk
   ];
